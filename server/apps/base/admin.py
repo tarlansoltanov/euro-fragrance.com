c882fbef9828aaf2c868817276f9dc6fabaddcd7
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-03-08 17:16:48 +0100

    Base Settings Model added and registered

--- server/apps/base/admin.py
+++ server/apps/base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import BaseSetting
 
-# Register your models here.
+
+@admin.register(BaseSetting)
+class BaseSettingAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'email', 'phone')
+    
+    def has_add_permission(self, request):
+        return not self.model.objects.count()
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
--- server/apps/base/models.py
+++ server/apps/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseSetting(models.Model):
+    """Model definition for BaseSetting."""
+
+    company_name = models.CharField(verbose_name="Company Name", max_length=255)
+    advert = models.CharField(verbose_name="Advertisement", max_length=50)
+    email = models.EmailField(verbose_name="Email", max_length=255)
+    phone = models.CharField(verbose_name="Phone", max_length=255)
+    facebook = models.CharField(verbose_name="Facebook", max_length=255)
+    instagram = models.CharField(verbose_name="Instagram", max_length=255)
+    address = models.CharField(verbose_name="Address", max_length=255)
+    address_url = models.CharField(verbose_name="Address URL", max_length=255)
+
+    class Meta:
+        """Meta definition for BaseSetting."""
+
+        verbose_name = 'BaseSetting'
+        verbose_name_plural = 'BaseSettings'
+
+    def __str__(self):
+        """Unicode representation of BaseSetting."""
+        return self.company_name
