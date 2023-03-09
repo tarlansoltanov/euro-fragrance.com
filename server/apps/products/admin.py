a90e79070ab90a8bb0e63ae75e8935290bbd74b9
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-03-09 18:35:34 +0100

    Product and Category models registered to admin

--- server/apps/products/admin.py
+++ server/apps/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.forms import ValidationError
+from .models import Product, ProductImage, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'categories')
+
+    def categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all()])
+    
+    categories.short_description = "Categories"
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'main')
+
+    def main(self, obj):
+        return obj.main.name if obj.main else None
