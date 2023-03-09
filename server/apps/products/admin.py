63b8b7c7501c4898247ec969f3e284715bc44c11
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-03-09 19:12:20 +0100

    Product Image Inline Form added to Product

--- server/apps/products/admin.py
+++ server/apps/products/admin.py
@@ -3,14 +3,22 @@ from django.forms import ValidationError
 from .models import Product, ProductImage, Category
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'categories')
+
+    inlines = [ProductImageInline]
 
     def categories(self, obj):
         return ", ".join([c.name for c in obj.categories.all()])
     
     categories.short_description = "Categories"
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -18,3 +26,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
     def main(self, obj):
         return obj.main.name if obj.main else None
+    
+    main.short_description = "Main"
